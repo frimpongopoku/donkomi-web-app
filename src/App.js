20e271c859402248800777cf07d2a54c80e88c8f
frimpongopoku <pongofrimi@gmail.com>
2021-12-30 21:08:04 +0000

    moved a few files around

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
-import logo from "./logo.svg";
-import "./shared/css/universal.css";
-import { bindActionCreators } from "redux";
-import { testReduxAction } from "./redux/actions/actions";
-import { connect } from "react-redux";
-import { useEffect } from "react";
+import React from "react";
 
-function App({ testAction, testStore }) {
-  useEffect(() => {
-    testAction(
-      "Welcome to your new app, redux, redux-thunk, and redux-logger are already setup. Enjoy!"
-    );
-  }, []);
-
-  console.log("This is from the store", testStore);
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Now this i sthe main component bro</h2>
     </div>
   );
 }
-
-const mapStateToProps = (state) => {
-  return {
-    testStore: state.testStore,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ testAction: testReduxAction }, dispatch);
-};
-export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./shared/css/universal.css";
 import Router from "./Router";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
