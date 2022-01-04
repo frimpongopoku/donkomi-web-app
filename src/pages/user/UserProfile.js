1ccb8a7bb44997b7420bd3edb33056181c05cb81
frimpongopoku <pongofrimi@gmail.com>
2022-01-04 10:22:42 +0000

    Added profile setting controls

--- src/pages/user/UserProfile.js
+++ src/pages/user/UserProfile.js
@@ -1,6 +1,9 @@
+import { faCaretRight, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import PageWrapper from "../wrapper/PageWrapper";
 import ImageThumbnail from "./../../components/thumbnail/ImageThumbnail";
+import { CONTROLS } from "./profile-values";
 import "./UserProfile.css";
 function UserProfile() {
   return (
@@ -17,6 +20,34 @@ function UserProfile() {
           </small>
         </div>
         <p className="profile-roles">@Delivery Guy, @Customer, @Seller</p>
+        <div className="controls-container">
+          {CONTROLS.map((c, index) => {
+            return (
+              <div
+                key={index.toString()}
+                className="one-control-option flex touchable-opacity"
+              >
+                <FontAwesomeIcon
+                  icon={c?.icon}
+                  style={{ marginRight: 20, color: "var(--app-color)" }}
+                />
+                <p>{c?.name}</p>
+                <span style={{ marginLeft: "auto" }}>
+                  <FontAwesomeIcon
+                    icon={faCaretRight}
+                    style={{ color: "var(--app-color-grey)" }}
+                  />
+                </span>
+              </div>
+            );
+          })}
+        </div>
+        <div style={{ width: "100%" }} className="setting-toggles">
+          <h3>Notification Settings</h3>
+          <div className="toggles-content">
+            
+          </div>
+        </div>
       </div>
     </PageWrapper>
   );
---
+++ src/pages/user/profile-values.js
@@ -0,0 +1,11 @@
+import {
+  faHandshake,
+  faSignOutAlt,
+  faUserAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const CONTROLS = [
+  { name: "Edit Profile", icon: faUserAlt },
+  { name: "Apply To Earn On Donkomi", icon: faHandshake },
+  { name: "Sign Out", icon: faSignOutAlt },
+];
