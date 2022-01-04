e1b0901fdaeb5f4a0e04737a3195fffd9ab6237e
frimpongopoku <pongofrimi@gmail.com>
2022-01-04 09:28:28 +0000

    user profile settings still ongoing - checkpoint

--- src/pages/user/UserProfile.js
+++ src/pages/user/UserProfile.js
@@ -1,10 +1,23 @@
 import React from "react";
 import PageWrapper from "../wrapper/PageWrapper";
-
+import ImageThumbnail from "./../../components/thumbnail/ImageThumbnail";
+import "./UserProfile.css";
 function UserProfile() {
   return (
     <PageWrapper>
-      <h1>I am the user profile</h1>
+      <div className="profile-container">
+        <ImageThumbnail className="profile-img" />
+        <h3>Frimpong Opoku Agyemang</h3>
+        <div className="profile-small">
+          <small style={{ color: "maroon" }} className="touchable-opacity">
+            Change
+          </small>{" "}
+          <small className="touchable-opacity" style={{ color: "green" }}>
+            Remove
+          </small>
+        </div>
+        <p className="profile-roles">@Delivery Guy, @Customer, @Seller</p>
+      </div>
     </PageWrapper>
   );
 }
