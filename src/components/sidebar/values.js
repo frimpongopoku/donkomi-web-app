b8f8a9efe917a7d77a8db02f07b6954306f7a1a2
frimpongopoku <pongofrimi@gmail.com>
2022-01-05 01:41:55 +0000

    pc mode settings page done

--- src/components/sidebar/values.js
+++ src/components/sidebar/values.js
@@ -1,9 +1,19 @@
-import { faCog, faHome, faMotorcycle, faQuestion, faTaxi } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCog,
+  faHome,
+  faMotorcycle,
+  faQuestion,
+  faShoppingBasket,
+  faSignOutAlt,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const MENU = [
-  {name: "Home", icon: faHome, key: "home"},
-  {name: "Merchant", icon: faMotorcycle, key: "merchant"},
-  {name: "Taxi", icon: faTaxi, key: "taxi"},
-  {name: "Help", icon: faQuestion, key: "help"},
-  {name: "Settings", icon: faCog, key: "settings"},
-]
+  { name: "Home", icon: faHome, key: "home" },
+  { name: "Market Place", icon: faShoppingBasket, key: "market-place" },
+  { name: "Merchant", icon: faMotorcycle, key: "merchant" },
+  { name: "Taxi", icon: faTaxi, key: "taxi" },
+  { name: "Help", icon: faQuestion, key: "help" },
+  { name: "Settings", icon: faCog, key: "settings" },
+  { name: "Logout", icon: faSignOutAlt, key: "logout" },
+];
