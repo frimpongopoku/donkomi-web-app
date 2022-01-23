a5aae89834796bdb27c13eccc490220dbef59d96
frimpongopoku <pongofrimi@gmail.com>
2022-01-23 11:18:39 +0400

    clearing up a few things here and there on UI

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Home.css";
 import {
@@ -9,80 +9,70 @@ import {
 import { useNavigate } from "react-router-dom";
 function Home() {
   const goto = useNavigate();
+  const [showJoinTeam, setShowJoinTeam] = useState(false);
   return (
-    <div className="home-container">
-      <div className="top-half">
+    <div>
+      <div className="home-back-image"></div>
+      <div className="home-floating-container"></div>
+      <div className="home-content-div">
         <h1>DONKOMI</h1>
+        <h3>Official ALC Market Place Online</h3>
+        <div
+          className="home-get-in-there touchable-opacity"
+          onClick={() => goto("/browse/market-place")}
+        >
+          <span>Get In There Now!</span>
+        </div>
       </div>
-      <div className="bottom-half">
-        <h3>
-          Welcome! <br /> We probably need a better intro but hey; <br />
-          buy, sell, order a taxi, become a merchant etc. Lots of stuff, just
-          check it out!
-        </h3>
-        <center>
-          <br />
-          <a
-            onClick={(e) => {
-              e.preventDefault();
-              goto("/browse/market-place");
-            }}
-            href="#void"
-            className="touchable-opacity"
-            style={{
-              background: "var(--app-color-darkest)",
-              color: "white",
-              padding: "10px 20px",
-              borderRadius: 55,
-              textDecoration: "none",
-            }}
-          >
-            Take Me There Now!{" "}
-            <span style={{ marginLeft: 6 }}>
-              <FontAwesomeIcon icon={faLongArrowAltRight} />
-            </span>
-          </a>
-          <br />
-          <br />
-          <a
-            href="mailto:donkomisales@gmail.com"
-            target="_blank"
-            style={{ color: "var(--app-color-darkest)" }}
-          >
-            <span style={{ marginRight: 3 }}>
-              <FontAwesomeIcon icon={faEnvelope} />
-            </span>
-            Have any intro suggestions? <br />
-            Please send here!
-          </a>
-          <br />
-          <br />
-          <a
-            href="mailto:mrfimpong@gmail.com"
-            target="_blank"
-            style={{ color: "var(--app-color-darkest)" }}
-          >
-            <span style={{ marginRight: 3 }}>
-              <FontAwesomeIcon icon={faEnvelope} />
-            </span>
-            Would you like to contribute to this project? <br />
-            Show you are interested via this email
-          </a>
 
-          <br />
-          <br />
-          <br />
-          <br />
-          <small style={{ color: "var(--app-color-darkest)" }}>
-            <b>
-              Made with <FontAwesomeIcon icon={faHeart} color="red" /> by
-              {` <ThatGuyInTheMaroonHoody />`}
-            </b>
+      {showJoinTeam && (
+        <div className="join-info-board anime-come-up">
+          <h5>
+            By now we are sure you've already figured out 4 million ways our
+            landing page could be better? <br />
+            Join the team and come do it yourself.
+          </h5>
+          <a href="mailto:donkomisales@gmail.com" target="_blank">
+            Send us an email here
+          </a>
+        </div>
+      )}
+      <div className="home-footer">
+        <div style={{ padding: "0px 15px" }}>
+          <small
+            style={{
+              fontSize: 11,
+              fontWeight: "bold",
+              color: "rgb(251 207 142 / 31%)",
+            }}
+          >
+            Made with{" "}
+            <FontAwesomeIcon icon={faHeart} style={{ marginRight: 4 }} />
+            by {"<MaroonHoody />"}
           </small>
-        </center>
+        </div>
+        <div
+          className="join-dev-team touchable-opacity"
+          onClick={() => setShowJoinTeam(!showJoinTeam)}
+        >
+          <small>Join Dev Team?</small>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Home;
+
+const PCHomePage = () => (
+  <div
+    style={{
+      background: "var(--app-color-darker)",
+      filter: "blur(10px)",
+      height: "100vh",
+      width: "100vw",
+    }}
+  ></div>
+);
+
+const MobileHomePage = () => <small>Another phone mode bro</small>;
